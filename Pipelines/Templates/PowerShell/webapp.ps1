73b5c1a2debcc07f69a7b4b3f0ea06b04beac577
unknown <santosh Kumar Mohapatra>
2023-12-22 18:01:03 +0000

    commit

--- Pipelines/Templates/PowerShell/webapp.ps1
+++ Pipelines/Templates/PowerShell/webapp.ps1
@@ -1,14 +1,14 @@
 # Defined the Web Application Name and location  
-$webappname="samplewebapp89"  
-$AppServicePlan="samplewebappplan892"  
-$location="Sweden Central" 
-$ResourceGroup="test-rg" 
+$WebAppName="samplewebapp89"  
+$AppServicePlan="samplewebappplan89"  
+$Location="Sweden Central" 
+$ResourceGroupName="test-rg" 
 
 # Create a resource group.
 # New-AzResourceGroup -Name myResourceGroup -Location $location
 
 # Create an App Service plan in Free tier.
-New-AzAppServicePlan -Name $webappname -Location $location -ResourceGroupName $ResourceGroup -Tier Free
+New-AzAppServicePlan -Name $AppServicePlan -Location $Location -ResourceGroupName $ResourceGroupName -Tier Free
 
 # Create a web app.
-New-AzWebApp -Name $webappname -Location $location -AppServicePlan $webappname -ResourceGroupName $ResourceGroup
+New-AzWebApp -Name $WebAppName -Location $Location -AppServicePlan $AppServicePlan -ResourceGroupName $ResourceGroupName
