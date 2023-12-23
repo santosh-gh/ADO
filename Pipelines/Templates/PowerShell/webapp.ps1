abdf9e8a96cccc94a492a245c0e6779a7130a1fc
unknown <santosh Kumar Mohapatra>
2023-12-23 16:33:24 +0000

    commit

--- Pipelines/Templates/PowerShell/webapp.ps1
+++ Pipelines/Templates/PowerShell/webapp.ps1
@@ -5,7 +5,7 @@ $Location="Sweden Central"
 $ResourceGroupName="test-rg" 
 
 # Create a resource group.
-# New-AzResourceGroup -Name myResourceGroup -Location $location
+New-AzResourceGroup -Name myResourceGroup -Location $location
 
 # Create an App Service plan in Free tier.
 New-AzAppServicePlan -Name $AppServicePlan -Location $Location -ResourceGroupName $ResourceGroupName -Tier Free
