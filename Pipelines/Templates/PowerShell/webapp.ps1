00df9eeb1ca0edeeb7bfb0b673d823a686076ac1
unknown <santosh Kumar Mohapatra>
2023-12-24 20:16:24 +0000

    commit

--- Pipelines/Templates/PowerShell/webapp.ps1
+++ Pipelines/Templates/PowerShell/webapp.ps1
@@ -5,10 +5,54 @@ $Location="Sweden Central"
 $ResourceGroupName="test-rg" 
 
 # Create a resource group.
-# New-AzResourceGroup -Name myResourceGroup -Location $location
+#$resourceGroup = New-AzResourceGroup -Name $ResourceGroupName -Location $Location
 
 # Create an App Service plan in Free tier.
 New-AzAppServicePlan -Name $AppServicePlan -Location $Location -ResourceGroupName $ResourceGroupName -Tier Free
 
 # Create a web app.
 New-AzWebApp -Name $WebAppName -Location $Location -AppServicePlan $AppServicePlan -ResourceGroupName $ResourceGroupName
+
+
+# Connect-AzAccount
+# The SubscriptionId in which to create these objects
+$SubscriptionId = 'b798c28b-e334-4ecf-b338-ec314ced3616'
+
+# Set an admin login and password for your server
+$adminSqlLogin = "SqlAdmin"
+$password = "Testdb@2023"
+
+# Set server name - the logical server name has to be unique in the system
+$serverName = "server-$(Get-Random)"
+
+# The sample database name
+$databaseName = "mySampleDatabase"
+
+# The ip address range that you want to allow to access your server
+$startIp = "0.0.0.0"
+$endIp = "0.0.0.0"
+
+# Set subscription 
+#Set-AzContext -SubscriptionId $subscriptionId 
+
+# Create a resource group
+#$resourceGroup = New-AzResourceGroup -Name $resourceGroupName -Location $location
+
+# Create a server with a system wide unique server name
+$server = New-AzSqlServer -ResourceGroupName $resourceGroupName `
+    -ServerName $serverName `
+    -Location $location `
+    -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminSqlLogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+
+# Create a server firewall rule that allows access from the specified IP range
+$serverFirewallRule = New-AzSqlServerFirewallRule -ResourceGroupName $resourceGroupName `
+    -ServerName $serverName `
+    -FirewallRuleName "AllowedIPs" -StartIpAddress $startIp -EndIpAddress $endIp
+
+# Create a blank database with an S0 performance level
+$database = New-AzSqlDatabase  -ResourceGroupName $resourceGroupName `
+    -ServerName $serverName `
+    -DatabaseName $databaseName `
+    -RequestedServiceObjectiveName "S0" `
+    -SampleName "AdventureWorksLT"
+
